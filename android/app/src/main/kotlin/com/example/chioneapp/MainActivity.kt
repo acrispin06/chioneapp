13df5f9c85a04bfef9214b722d57d7a72f8fde4f
ALVARO <alvarocrispin0604@gmail.com>
2024-11-08 15:35:34 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/chioneapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chioneapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'viewmodels/budget_view_model.dart';
+import 'viewmodels/category_view_model.dart';
+import 'viewmodels/goal_view_model.dart';
+import 'viewmodels/notification_view_model.dart';
+import 'viewmodels/report_view_model.dart';
+import 'viewmodels/transaction_view_model.dart';
+import 'viewmodels/user_view_model.dart';
+import 'views/budget_screen.dart';
+import 'views/home_screen.dart';
+//import 'views/notification_screen.dart';
+//import 'views/report_screen.dart';
+import 'views/transaction_screen.dart';
+//import 'views/profile_screen.dart';
+
+void main() {
+  runApp(ChiOneApp());
+}
+
+class ChiOneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BudgetViewModel()),
+        ChangeNotifierProvider(create: (_) => CategoryViewModel()),
+        ChangeNotifierProvider(create: (_) => GoalViewModel()),
+        ChangeNotifierProvider(create: (_) => NotificationViewModel()),
+        ChangeNotifierProvider(create: (_) => ReportViewModel()),
+        ChangeNotifierProvider(create: (_) => TransactionViewModel()),
+        ChangeNotifierProvider(create: (_) => UserViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ChiOneApp',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: MainNavigationScreen(),
+      ),
+    );
+  }
+}
+
+class MainNavigationScreen extends StatefulWidget {
+  @override
+  _MainNavigationScreenState createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    //ReportScreen(),
+    TransactionScreen(),
+    BudgetScreen(),
+    //ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Reports'),
+          BottomNavigationBarItem(icon: Icon(Icons.swap_horiz), label: 'Transactions'),
+          BottomNavigationBarItem(icon: Icon(Icons.category), label: 'Budgets'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
