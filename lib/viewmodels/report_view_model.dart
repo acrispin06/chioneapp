90bd9531de702c3e5a0391f96b7c58f4bd3344cb
ALVARO <alvarocrispin0604@gmail.com>
2024-11-09 22:14:32 -0500

    feat(report): adding call to category id

--- lib/viewmodels/report_view_model.dart
+++ lib/viewmodels/report_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:chioneapp/models/transaction.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import '../db/database_helper.dart';
 import '../models/report.dart';
@@ -5,8 +7,11 @@ import '../models/report.dart';
 class ReportViewModel extends ChangeNotifier {
   final DatabaseHelper _dbHelper = DatabaseHelper();
   List<Report> _reports = [];
+  List<Transaction> _transactions = [];
+  String _selectedPeriod = "Daily";
 
   List<Report> get reports => _reports;
+  String get selectedPeriod => _selectedPeriod;
 
   Future<void> fetchReports() async {
     _reports = await _dbHelper.getReports();
@@ -16,5 +21,43 @@ class ReportViewModel extends ChangeNotifier {
   Future<void> addReport(Report report) async {
     await _dbHelper.insertReport(report);
     await fetchReports();
+  }
+
+  // Método para obtener el balance total
+  double getTotalBalance() {
+    double totalIncome = getTotalIncome();
+    double totalExpense = getTotalExpense();
+    return totalIncome - totalExpense;
+  }
+
+  // Método para obtener el gasto total
+  double getTotalExpense() {
+    return _transactions
+        .where((transaction) => transaction.type == 'expense')
+        .fold(0.0, (sum, transaction) => sum + transaction.amount);
+  }
+
+  // Método para obtener el ingreso total
+  double getTotalIncome() {
+    return _transactions
+        .where((transaction) => transaction.type == 'income')
+        .fold(0.0, (sum, transaction) => sum + transaction.amount);
+  }
+
+  // Método para obtener los puntos de datos para el gráfico de ingresos y gastos
+  List<FlSpot> getIncomeExpenseSpots() {
+    List<FlSpot> spots = [];
+    for (int i = 0; i < _transactions.length; i++) {
+      final transaction = _transactions[i];
+      double amount = transaction.type == 'income' ? transaction.amount : -transaction.amount;
+      spots.add(FlSpot(i.toDouble(), amount));
+    }
+    return spots;
+  }
+
+  // Cambiar el período seleccionado (Daily, Weekly, Monthly, Yearly)
+  void changePeriod(String period) {
+    _selectedPeriod = period;
+    notifyListeners();
   }
 }
