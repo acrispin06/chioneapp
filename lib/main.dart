7a860da6b0aa2482d1512a8238ed36b43bbb406c
ALVARO <alvarocrispin0604@gmail.com>
2024-12-03 17:05:40 -0500

    feat(main): new main with shared

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:chioneapp/db/database_helper.dart';
-import 'package:chioneapp/views/report_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'views/home_screen.dart';
 import 'viewmodels/budget_view_model.dart';
 import 'viewmodels/category_view_model.dart';
 import 'viewmodels/goal_view_model.dart';
@@ -9,19 +8,8 @@ import 'viewmodels/notification_view_model.dart';
 import 'viewmodels/report_view_model.dart';
 import 'viewmodels/transaction_view_model.dart';
 import 'viewmodels/user_view_model.dart';
-import 'views/budget_screen.dart';
-import 'views/home_screen.dart';
-//import 'views/notification_screen.dart';
-//import 'views/report_screen.dart';
-import 'views/transaction_screen.dart';
-//import 'views/profile_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final dbHelper = DatabaseHelper();
- // await dbHelper.insertSampleData();
-
+void main() {
   runApp(ChiOneApp());
 }
 
@@ -43,52 +31,9 @@ class ChiOneApp extends StatelessWidget {
         title: 'ChiOneApp',
         theme: ThemeData(
           primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: MainNavigationScreen(),
-      ),
-    );
-  }
-}
-
-class MainNavigationScreen extends StatefulWidget {
-  @override
-  _MainNavigationScreenState createState() => _MainNavigationScreenState();
-}
-
-class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _screens = [
-    HomeScreen(),
-    ReportScreen(),
-    TransactionScreen(),
-    BudgetScreen(),
-    //ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Reports'),
-          BottomNavigationBarItem(icon: Icon(Icons.swap_horiz), label: 'Transactions'),
-          BottomNavigationBarItem(icon: Icon(Icons.category), label: 'Budgets'),
-          //BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
+        home: HomeScreen(),
       ),
     );
   }
