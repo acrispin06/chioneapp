0d4387ef4df0a606dbaff8cc333aae0ab3485d2a
ALVARO <alvarocrispin0604@gmail.com>
2024-12-03 16:59:28 -0500

    feat(db): adding table transactions and solving errors in compile

--- lib/db/database_helper.dart
+++ lib/db/database_helper.dart
@@ -29,222 +29,225 @@ class DatabaseHelper {
 
   Future<void> _onCreate(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE currencies(
-        currency_id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        symbol TEXT NOT NULL
-      )
+    CREATE TABLE IF NOT EXISTS currencies (
+      currency_id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      symbol TEXT NOT NULL
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE transaction_types(
-        type_id INTEGER PRIMARY KEY,
-        type_name TEXT NOT NULL UNIQUE
-      )
+    CREATE TABLE IF NOT EXISTS transaction_types (
+      type_id INTEGER PRIMARY KEY,
+      type_name TEXT NOT NULL UNIQUE
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE users(
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        currency_id INTEGER,
-        budgetGoal REAL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (currency_id) REFERENCES currencies(currency_id)
-      )
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      currency_id INTEGER,
+      budgetGoal REAL,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (currency_id) REFERENCES currencies(currency_id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE icons(
-        icon_id INTEGER PRIMARY KEY,
-        icon_name TEXT NOT NULL,
-        icon_path TEXT
-      )
+    CREATE TABLE IF NOT EXISTS icons (
+      icon_id INTEGER PRIMARY KEY,
+      icon_name TEXT NOT NULL,
+      icon_path TEXT
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE categories(
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        type INTEGER,
-        icon_id INTEGER,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (type) REFERENCES transaction_types(type_id),
-        FOREIGN KEY (icon_id) REFERENCES icons(icon_id)
-      )
+    CREATE TABLE IF NOT EXISTS categories (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      type INTEGER,
+      icon_id INTEGER,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (type) REFERENCES transaction_types(type_id),
+      FOREIGN KEY (icon_id) REFERENCES icons(icon_id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE goal_categories(
-        goal_id INTEGER,
-        category_id INTEGER,
-        PRIMARY KEY (goal_id, category_id),
-        FOREIGN KEY (goal_id) REFERENCES goals(id),
-        FOREIGN KEY (category_id) REFERENCES categories(id)
-      )
+    CREATE TABLE IF NOT EXISTS goal_categories (
+      goal_id INTEGER,
+      category_id INTEGER,
+      PRIMARY KEY (goal_id, category_id),
+      FOREIGN KEY (goal_id) REFERENCES goals(id),
+      FOREIGN KEY (category_id) REFERENCES categories(id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE incomes(
-        id INTEGER PRIMARY KEY,
-        amount REAL NOT NULL,
-        category_id INTEGER,
-        date DATE NOT NULL,
-        time TIME NOT NULL,
-        description TEXT,
-        icon_id INTEGER,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (category_id) REFERENCES categories(id),
-        FOREIGN KEY (icon_id) REFERENCES icons(icon_id)
-      )
+    CREATE TABLE IF NOT EXISTS incomes (
+      id INTEGER PRIMARY KEY,
+      amount REAL NOT NULL,
+      category_id INTEGER,
+      date DATE NOT NULL,
+      time TIME NOT NULL,
+      description TEXT,
+      icon_id INTEGER,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (category_id) REFERENCES categories(id),
+      FOREIGN KEY (icon_id) REFERENCES icons(icon_id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE expenses(
-        id INTEGER PRIMARY KEY,
-        amount REAL NOT NULL,
-        category_id INTEGER,
-        date DATE NOT NULL,
-        time TIME NOT NULL,
-        description TEXT,
-        icon_id INTEGER,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (category_id) REFERENCES categories(id),
-        FOREIGN KEY (icon_id) REFERENCES icons(icon_id)
-      )
+    CREATE TABLE IF NOT EXISTS expenses (
+      id INTEGER PRIMARY KEY,
+      amount REAL NOT NULL,
+      category_id INTEGER,
+      date DATE NOT NULL,
+      time TIME NOT NULL,
+      description TEXT,
+      icon_id INTEGER,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (category_id) REFERENCES categories(id),
+      FOREIGN KEY (icon_id) REFERENCES icons(icon_id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE budgets(
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-        category_id INTEGER,
-        amount REAL NOT NULL,
-        date DATE NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (category_id) REFERENCES categories(id)
-      )
+    CREATE TABLE IF NOT EXISTS budgets (
+      id INTEGER PRIMARY KEY,
+      user_id INTEGER,
+      category_id INTEGER,
+      amount REAL NOT NULL,
+      date TEXT NOT NULL,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (user_id) REFERENCES users(id),
+      FOREIGN KEY (category_id) REFERENCES categories(id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE goals(
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-        name TEXT NOT NULL,
-        amount REAL NOT NULL,
-        currentAmount REAL DEFAULT 0.0,
-        targetDate DATE NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id)
-      )
+    CREATE TABLE IF NOT EXISTS goals (
+      id INTEGER PRIMARY KEY,
+      user_id INTEGER,
+      name TEXT NOT NULL,
+      amount REAL NOT NULL,
+      currentAmount REAL DEFAULT 0.0,
+      targetDate TEXT NOT NULL,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (user_id) REFERENCES users(id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE goal_transactions(
-        id INTEGER PRIMARY KEY,
-        goal_id INTEGER,
-        transaction_id INTEGER,
-        progress_percentage REAL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (goal_id) REFERENCES goals(id),
-        FOREIGN KEY (transaction_id) REFERENCES incomes(id)
-      )
+    CREATE TABLE IF NOT EXISTS goal_transactions (
+      id INTEGER PRIMARY KEY,
+      goal_id INTEGER,
+      transaction_id INTEGER,
+      progress_percentage REAL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (goal_id) REFERENCES goals(id),
+      FOREIGN KEY (transaction_id) REFERENCES incomes(id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE user_category_preferences(
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-        category_id INTEGER,
-        preferred_budget REAL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (category_id) REFERENCES categories(id)
-      )
+    CREATE TABLE IF NOT EXISTS user_category_preferences (
+      id INTEGER PRIMARY KEY,
+      user_id INTEGER,
+      category_id INTEGER,
+      preferred_budget REAL,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (user_id) REFERENCES users(id),
+      FOREIGN KEY (category_id) REFERENCES categories(id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE audit_logs(
-        id INTEGER PRIMARY KEY,
-        table_name TEXT NOT NULL,
-        record_id INTEGER NOT NULL,
-        changed_field TEXT NOT NULL,
-        old_value TEXT,
-        new_value TEXT,
-        change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        changed_by INTEGER,
-        FOREIGN KEY (changed_by) REFERENCES users(id)
-      )
+    CREATE TABLE IF NOT EXISTS audit_logs (
+      id INTEGER PRIMARY KEY,
+      table_name TEXT NOT NULL,
+      record_id INTEGER NOT NULL,
+      changed_field TEXT NOT NULL,
+      old_value TEXT,
+      new_value TEXT,
+      change_date TEXT DEFAULT (datetime('now')),
+      changed_by INTEGER,
+      FOREIGN KEY (changed_by) REFERENCES users(id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE notifications(
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        message TEXT NOT NULL,
-        date DATE NOT NULL,
-        type TEXT,
-        entity_id INTEGER,
-        entity_type TEXT,
-        status TEXT DEFAULT 'unread',
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-      )
+    CREATE TABLE IF NOT EXISTS notifications (
+      id INTEGER PRIMARY KEY,
+      title TEXT NOT NULL,
+      message TEXT NOT NULL,
+      date TEXT NOT NULL,
+      type TEXT,
+      entity_id INTEGER,
+      entity_type TEXT,
+      status TEXT DEFAULT 'unread',
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now'))
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE notification_types(
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
-      )
+    CREATE TABLE IF NOT EXISTS notification_types (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL
+    );
     ''');
 
     await db.execute('''
-      CREATE VIEW category_spent AS
-      SELECT category_id AS categoryId, SUM(amount) AS spent
-      FROM expenses
-      GROUP BY category_id
+    CREATE TABLE IF NOT EXISTS transactions (
+      id INTEGER PRIMARY KEY,
+      amount REAL NOT NULL,
+      category_id INTEGER,
+      type_id INTEGER NOT NULL,
+      date TEXT NOT NULL,
+      time TEXT NOT NULL,
+      description TEXT,
+      icon_id INTEGER,
+      created_at TEXT DEFAULT (datetime('now')),
+      updated_at TEXT DEFAULT (datetime('now')),
+      FOREIGN KEY (category_id) REFERENCES categories(id),
+      FOREIGN KEY (icon_id) REFERENCES icons(icon_id),
+      FOREIGN KEY (type_id) REFERENCES transaction_types(type_id)
+    );
     ''');
 
     await db.execute('''
-      CREATE TABLE daily_reports(
-        id INTEGER PRIMARY KEY,
-        date DATE NOT NULL,
-        totalIncome REAL,
-        totalExpense REAL,
-        balance REAL
-      )
+    CREATE VIEW IF NOT EXISTS category_spent AS
+    SELECT category_id AS categoryId, SUM(amount) AS spent
+    FROM transactions
+    WHERE type_id = 2
+    GROUP BY category_id
     ''');
 
     await db.execute('''
-      CREATE TABLE monthly_reports(
-        id INTEGER PRIMARY KEY,
-        month INTEGER NOT NULL,
-        year INTEGER NOT NULL,
-        totalIncome REAL,
-        totalExpense REAL,
-        balance REAL
-      )
+    CREATE TABLE IF NOT EXISTS reports (
+    id INTEGER PRIMARY KEY,
+    period_type TEXT NOT NULL,
+    start_date TEXT NOT NULL,
+    end_date TEXT NOT NULL,
+    total_income REAL DEFAULT 0.0,
+    total_expense REAL DEFAULT 0.0,
+    balance REAL GENERATED ALWAYS AS (total_income - total_expense) VIRTUAL,
+    created_at TEXT DEFAULT (datetime('now')),
+    updated_at TEXT DEFAULT (datetime('now')),
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    );
     ''');
-
-    await db.execute('''
-      CREATE TABLE yearly_reports(
-        id INTEGER PRIMARY KEY,
-        year INTEGER NOT NULL,
-        totalIncome REAL,
-        totalExpense REAL,
-        balance REAL
-      )
-    ''');
-
     // Insert example transaction types and notification types
     //inserting default values
     //Currencies
@@ -272,24 +275,24 @@ class DatabaseHelper {
     await db.insert('categories', {'name': 'Education', 'type': 2, 'icon_id': 7});
     await db.insert('categories', {'name': 'Others', 'type': 2, 'icon_id': 8});
     //budgets
-    await db.insert('budgets', {'user_id': 1, 'category_id': 1, 'amount': 100.0, 'date': '2024-11-31'});
-    await db.insert('budgets', {'user_id': 1, 'category_id': 2, 'amount': 50.0, 'date': '2024-11-31'});
-    await db.insert('budgets', {'user_id': 1, 'category_id': 3, 'amount': 200.0, 'date': '2024-11-31'});
-    await db.insert('budgets', {'user_id': 1, 'category_id': 4, 'amount': 150.0, 'date': '2024-11-31'});
-    await db.insert('budgets', {'user_id': 1, 'category_id': 5, 'amount': 100.0, 'date': '2024-11-31'});
-    await db.insert('budgets', {'user_id': 1, 'category_id': 6, 'amount': 50.0, 'date': '2024-11-31'});
-    await db.insert('budgets', {'user_id': 1, 'category_id': 7, 'amount': 50.0, 'date': '2024-11-31'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 1, 'amount': 100.0, 'date': '2024-11-29'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 2, 'amount': 50.0, 'date': '2024-11-29'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 3, 'amount': 200.0, 'date': '2024-11-29'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 4, 'amount': 150.0, 'date': '2024-11-29'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 5, 'amount': 100.0, 'date': '2024-11-29'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 6, 'amount': 50.0, 'date': '2024-11-29'});
+    await db.insert('budgets', {'user_id': 1, 'category_id': 7, 'amount': 50.0, 'date': '2024-11-29'});
     //incomes
-    await db.insert('incomes', {'amount': 100.0, 'category_id': 1, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Income 1', 'icon_id': 2});
-    await db.insert('incomes', {'amount': 200.0, 'category_id': 2, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Income 2', 'icon_id': 3});
-    await db.insert('incomes', {'amount': 300.0, 'category_id': 3, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Income 3', 'icon_id': 4});
-    await db.insert('incomes', {'amount': 400.0, 'category_id': 4, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Income 4', 'icon_id': 5});
-    await db.insert('incomes', {'amount': 500.0, 'category_id': 5, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Income 5', 'icon_id': 6});
-    await db.insert('incomes', {'amount': 600.0, 'category_id': 6, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Income 6', 'icon_id': 7});
+    await db.insert('incomes', {'amount': 100.0, 'category_id': 1, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Income 1', 'icon_id': 2});
+    await db.insert('incomes', {'amount': 200.0, 'category_id': 2, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Income 2', 'icon_id': 3});
+    await db.insert('incomes', {'amount': 300.0, 'category_id': 3, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Income 3', 'icon_id': 4});
+    await db.insert('incomes', {'amount': 400.0, 'category_id': 4, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Income 4', 'icon_id': 5});
+    await db.insert('incomes', {'amount': 500.0, 'category_id': 5, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Income 5', 'icon_id': 6});
+    await db.insert('incomes', {'amount': 600.0, 'category_id': 6, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Income 6', 'icon_id': 7});
     //expenses
-    await db.insert('expenses', {'amount': 100.0, 'category_id': 1, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Expense 1', 'icon_id': 2});
-    await db.insert('expenses', {'amount': 200.0, 'category_id': 2, 'date': '2024-1131', 'time': '12:00:00', 'description': 'Expense 2', 'icon_id': 3});
-    await db.insert('expenses', {'amount': 300.0, 'category_id': 3, 'date': '2024-11-31', 'time': '12:00:00', 'description': 'Expense 3', 'icon_id': 4});
+    await db.insert('expenses', {'amount': 100.0, 'category_id': 1, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Expense 1', 'icon_id': 2});
+    await db.insert('expenses', {'amount': 200.0, 'category_id': 2, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Expense 2', 'icon_id': 3});
+    await db.insert('expenses', {'amount': 300.0, 'category_id': 3, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Expense 3', 'icon_id': 4});
     //goals
     await db.insert('goals', {'user_id': 1, 'name': 'Buy a new car', 'amount': 10000.0, 'targetDate': '2022-12-31'});
     await db.insert('goals', {'user_id': 1, 'name': 'Buy a new house', 'amount': 50000.0, 'targetDate': '2025-12-31'});
@@ -309,6 +312,8 @@ class DatabaseHelper {
     await db.insert('user_category_preferences', {'user_id': 1, 'category_id': 5, 'preferred_budget': 100.0});
     await db.insert('user_category_preferences', {'user_id': 1, 'category_id': 6, 'preferred_budget': 50.0});
     await db.insert('user_category_preferences', {'user_id': 1, 'category_id': 7, 'preferred_budget': 50.0});
+    await db.insert('transactions', {'amount': 100.0, 'category_id': 1, 'type_id': 1, 'date': '2024-11-29', 'time': '12:00:00', 'description': 'Salary Income', 'icon_id': 2});
+    await db.insert('transactions', {'amount': 50.0, 'category_id': 2, 'type_id': 2, 'date': '2024-11-29', 'time': '14:00:00', 'description': 'Grocery Shopping', 'icon_id': 3});
   }
   //CRUD Operations for each model
   //Crud for user
