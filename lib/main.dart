78bb6f7def6472874f9491cf6e25c9474532d092
ALVARO <alvarocrispin0604@gmail.com>
2024-12-14 11:00:12 -0500

    feat(main): ensuring that db is created and the example data is inserted

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chioneapp/db/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'views/home_screen.dart';
@@ -9,11 +10,17 @@ import 'viewmodels/report_view_model.dart';
 import 'viewmodels/transaction_view_model.dart';
 import 'viewmodels/user_view_model.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final dbHelper = DatabaseHelper();
+  await dbHelper.database;
+  //await dbHelper.createExampleData();
   runApp(ChiOneApp());
 }
 
 class ChiOneApp extends StatelessWidget {
+  const ChiOneApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
