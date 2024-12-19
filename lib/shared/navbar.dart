2d4863f351030330968bac0f40786d9232dc8bde
acrispin06 <alvarocrispin0604@gmail.com>
2024-12-19 02:53:02 -0500

    feature(navbar): Improving the UI design

--- lib/shared/navbar.dart
+++ lib/shared/navbar.dart
@@ -4,34 +4,79 @@ class BottomNavBar extends StatelessWidget {
   final int currentIndex;
   final Function(int) onTap;
 
-  const BottomNavBar({super.key, required this.currentIndex, required this.onTap});
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
+    final Color primaryColor = Theme.of(context).colorScheme.primary;
+    final Color unselectedColor = Colors.grey;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: onTap,
-        items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-        icon: Icon(Icons.account_balance_wallet_rounded),
-          label: 'Budgets',
-        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        selectedItemColor: primaryColor,
+        unselectedItemColor: unselectedColor,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.category),
+            icon: _buildIcon(Icons.home, 0),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.account_balance_wallet_rounded, 1),
+            label: 'Budgets',
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.category, 2),
             label: 'Goals',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.pie_chart),
+            icon: _buildIcon(Icons.pie_chart, 3),
             label: 'Reports',
           ),
         ],
-      selectedItemColor: Colors.green,
-      unselectedItemColor: Colors.grey,
-      type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  Widget _buildIcon(IconData icon, int index) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: currentIndex == index
+            ? Colors.green.withOpacity(0.1)
+            : Colors.transparent,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Icon(
+        icon,
+        size: 24,
+        color: currentIndex == index ? Colors.green : Colors.grey,
+      ),
     );
   }
 }
